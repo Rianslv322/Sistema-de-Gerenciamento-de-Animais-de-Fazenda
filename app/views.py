ff1e5cffb9ed1e0a9fbccb623f792efb309d5fb0
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2026-02-25 21:19:53 -0300

    feat: arquivo base.html e configurações de rotas

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
-@app.route('/')
+@app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
